d9eff1a7779d8d645d7108686f99e9a8af0bf2d8
j02on <pjylove08@naver.com>
2026-02-12 10:04:30 +0900

    fix : cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173'];
 
 app.use(
   cors({
